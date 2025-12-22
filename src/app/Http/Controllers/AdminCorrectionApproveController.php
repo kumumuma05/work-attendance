17d537670ba77af3f346db64c0add430e5c1c0aa
kumub <ma.okamoto.yf7@gmail.com>
2025-12-23 00:47:18 +0900

    修正申請実行の修正

--- src/app/Http/Controllers/AdminCorrectionApproveController.php
+++ src/app/Http/Controllers/AdminCorrectionApproveController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\AttendanceRequest;
 use Carbon\Carbon;
+use App\Models\Attendance;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class AdminCorrectionApproveController extends Controller
 {
@@ -45,7 +48,7 @@ class AdminCorrectionApproveController extends Controller
     {
         DB::transaction (function () use ($attendance_correct_request_id) {
             // 対象の申請データ特定
-            $attendanceRequest = AttendanceRequest::with(['attendance.user'])
+            $request = AttendanceRequest::with(['attendance.user'])
             ->findOrFail($attendance_correct_request_id);
 
             // 二重承認防止
@@ -53,17 +56,31 @@ class AdminCorrectionApproveController extends Controller
                 return;
             }
 
-            // attendancsテーブル書き換え
-            $attendance => $request->attendance;
-            $attendance->updadte([
+            // attendancesテーブル書き換え
+            $attendance = $request->attendance;
+            $attendance->update([
                 'clock_in' => $request->requested_clock_in,
                 'clock_out' => $request->requested_clock_out,
-            ])
+            ]);
 
             // breaksテーブル書き換え
             $attendance->breaks()->delete();
+            foreach ($request->requested_breaks as $break) {
+                if ($break['break_in'] && $break['break_out']) {
+                    $attendance->breaks()->create([
+                        'break_in' => $break['break_in'],
+                        'break_out' => $break['break_out'],
+                    ]);
+                }
+            }
+            // 修正申請を承認済みに変更
+            $request->update([
+                'status' => 'approved',
+                'approved_by' => Auth::guard('admin')->id(),
+                'approved_at' => now(),
+            ]);
+        });
 
-
-        })
-    } 
+        return redirect()->back();
+    }
 }
