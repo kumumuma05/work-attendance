aac9becd031fc567c939e5e1b3ee9ce4b00d1c48
kumub <ma.okamoto.yf7@gmail.com>
2026-01-15 20:23:17 +0900

    ミドルコード作成

--- src/app/Http/Controllers/StampCorrectionRequestEntryController.php
+++ src/app/Http/Controllers/StampCorrectionRequestEntryController.php
@@ -6,5 +6,12 @@ use Illuminate\Http\Request;
 
 class StampCorrectionRequestEntryController extends Controller
 {
-    //
+    public function index(
+        CorrectionRequestController $userController,
+        AdminCorrectionRequestController $adminController
+    ) {
+        return Auth::guard('admin')->check()
+            ? $adminController->index(request())
+            : $userController->index(request());
+    }
 }
