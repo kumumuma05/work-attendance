c68b99b8df6bc2d6d60d38e996060fabaeec51b8
kumub <ma.okamoto.yf7@gmail.com>
2026-01-15 23:21:31 +0900

    test11-3

--- src/tests/Feature/AttendanceDetailCorrectionTest.php
+++ src/tests/Feature/AttendanceDetailCorrectionTest.php
@@ -72,4 +72,39 @@ class AttendanceDetailCorrectionTest extends TestCase
             "requested_breaks.{$break->id}.break_in" => '休憩時間が不適切な値です',
         ]);
     }
+
+    public function test_error_message_is_displayed_when_break_out_is_after_clock_out(){
+        // 勤怠情報が登録されたユーザーにログインする
+        $user = User::factory()->create();
+        $attendance = Attendance::factory()->create([
+            'user_id' => $user->id,
+            'clock_in' => Carbon::create(2026, 1, 5, 9, 0),
+            'clock_out' => Carbon::create(2026, 1, 5, 18, 0),
+        ]);
+        $break = $attendance->breaks()->create([
+            'break_in' => Carbon::create(2026, 1, 5, 12, 0),
+            'break_out' => Carbon::create(2026, 1, 5, 13, 0),
+        ]);
+        $this->actingAs($user, 'web');
+
+        // 勤怠詳細ページを開く
+        $response = $this->get("/attendance/detail/{$attendance->id}");
+        $response->assertStatus(200);
+
+        // 休憩開始時間を退勤時間より後に設定する
+        $response = $this->post("/attendance/detail/{$attendance->id}", [
+            'requested_clock_out' => '18:00',
+                'requested_breaks' => [
+                $break->id => [
+                    'break_in' => '12:00',
+                    'break_out' => '20:00',
+                ],
+            ],
+        ]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHasErrors([
+            "requested_breaks.{$break->id}.break_out" => '休憩時間もしくは退勤時間が不適切な値です',
+        ]);
+    }
 }
