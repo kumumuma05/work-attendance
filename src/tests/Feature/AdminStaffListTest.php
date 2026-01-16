14c80de218f5b75fccd38c0c3ecba92325408ba2
kumub <ma.okamoto.yf7@gmail.com>
2026-01-16 17:36:23 +0900

    test14-2

--- src/tests/Feature/AdminStaffListTest.php
+++ src/tests/Feature/AdminStaffListTest.php
@@ -7,6 +7,8 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Admin;
+use App\Models\Attendance;
+use Carbon\Carbon;
 
 class AdminStaffListTest extends TestCase
 {
@@ -40,5 +42,36 @@ class AdminStaffListTest extends TestCase
         $response->assertSee('user2@test.com');
         $response->assertSee('user3');
         $response->assertSee('user3@test.com');
+    }
+
+    /**
+     * ユーザーの勤怠情報が正しく表示されることを確認
+     */
+    public function test_admin_sees_users_attendance_information_correctly() {
+        // 準備
+        Carbon::setTestNow(Carbon::create(2026, 1, 5));
+        $user = User::factory()->create([
+            'name' => 'user1',
+        ]);
+        $attendance = Attendance::factory()->create([
+            'user_id' => $user->id,
+            'clock_in' => Carbon::create(2026, 1, 5, 9, 0),
+            'clock_out' => Carbon::create(2026, 1, 5, 18, 0),
+        ]);
+
+        // 管理者でログインする
+        $admin =Admin::factory()->create();
+        $this->actingAs($admin, 'admin');
+
+        // 選択したユーザーの勤怠一覧ページを開く
+        $response = $this->get("/admin/attendance/staff/{$user->id}");
+        $response->assertStatus(200);
+
+        $response->assertSee('user1さんの勤怠');
+        $response->assertSee('01/05');
+        $response->assertSee('09:00');
+        $response->assertSee('18:00');
+
+        Carbon::setTestNow();
     }
 }
