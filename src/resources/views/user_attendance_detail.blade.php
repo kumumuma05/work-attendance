427b7d8f3549968e660dfefa49e67983e446a2aa
kumub <ma.okamoto.yf7@gmail.com>
2026-01-18 19:21:17 +0900

    userditailcheck

--- src/resources/views/user_attendance_detail.blade.php
+++ src/resources/views/user_attendance_detail.blade.php
@@ -8,13 +8,13 @@
     <link rel="stylesheet" href="{{ asset('css/user_attendance_detail.css') }}">
 @endsection
 
+<!-- ヘッダー -->
+@section('header')
+    @include('header.user_working')
+@endsection
+
 <!-- 本体 -->
 @section('content')
-    @php
-        $readonly = $hasPendingRequest;
-    @endphp
-
-    @include('header.user_working')
     <div class="attendance-detail">
         <!-- タイトル -->
         <h1 class="attendance-detail__title">
@@ -24,7 +24,7 @@
         <!-- 勤務詳細 -->
         <form class="attendance-detail__form" action="/attendance/detail/{{ $attendance->id }}" method="post" novalidate>
             @csrf
-            <div class="attendance-detail__card">
+            <dl class="attendance-detail__card">
                 <!-- 名前 -->
                 <div class="attendance-detail__row">
                     <dt class="attendance-detail__term">名前</dt>
@@ -124,9 +124,7 @@
                     <dd class="attendance-detail__data">
                         <div class="attendance-detail__data-wrap">
                             @if (!$hasPendingRequest)
-                                <textarea class="attendance-detail__remark-text" name="remarks">
-                                    {{ old('remarks', $attendance->remarks ?? '') }}
-                                </textarea>
+                                <textarea class="attendance-detail__remark-text" name="remarks">{{ old('remarks', $attendance->remarks ?? '') }}</textarea>
                             @else
                                 <div class="attendance-detail__remark">
                                     {{ $pendingRequest->remarks }}
@@ -140,7 +138,7 @@
                         </div>
                     </dd>
                 </div>
-            </div>
+            </dl>
 
             <div class="attendance-detail__button">
                 @if (!$hasPendingRequest)
